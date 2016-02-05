0bd00ff8052a8483aac690760088c18741929282
avalkov <avalkov@mail.bg>
2016-02-05 04:24:11 -0800

    Added JSONModel
    Added JSONModel

---
+++ Client/IParty/IParty/CreatePartyRequestModel.h
@@ -0,0 +1,15 @@
+//
+//  CreatePartyRequestModel.h
+//  IParty
+//
+//  Created by Swifty on 2/5/16.
+//  Copyright © 2016 Swifty. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "JSONModel/JSONModel.h"
+
+@interface CreatePartyRequestModel : NSObject
+
+@end
---
+++ Client/IParty/IParty/CreatePartyRequestModel.m
@@ -0,0 +1,13 @@
+//
+//  CreatePartyRequestModel.m
+//  IParty
+//
+//  Created by Swifty on 2/5/16.
+//  Copyright © 2016 Swifty. All rights reserved.
+//
+
+#import "CreatePartyRequestModel.h"
+
+@implementation CreatePartyRequestModel
+
+@end
